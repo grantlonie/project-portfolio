a3410e858d112ad0c504af17c47291e9b2e104d6
GeeDollaHolla <grantlonie@gmail.com>
2019-01-01 17:29:33 -0600

    refractor: change all redux logic over to immer

--- src/js/reducers.js
+++ src/js/reducers.js
@@ -11,31 +11,74 @@ const initialState = {
 const rootReducer = produce((draft = initialState, action) => {
 	switch (action.type) {
 		case 'ADD_CATEGORY':
-			return addCategory(draft, action)
+			draft.allCategories.push(action.category)
+			return
 
 		case 'ADD_PROJECT':
-			return { ...draft, projects: [...draft.projects, action.project] }
+			draft.projects.push(action.project)
+			return
 
 		case 'ADD_SKILL':
-			return addSkill(draft, action)
+			draft.allSkills.push(action.skill)
+			return
 
 		case 'ADD_SKILL_TO_PROJECT':
-			return addSkillToProject(draft, action)
+			draft.projects = draft.projects.map(project => {
+				if (project.id === action.skill.project.id) project.skills.items.push(action.skill)
+				return project
+			})
+			return
 
 		case 'ADD_TOOL_TO_SKILL':
-			return addToolToSkill(draft, action)
-
-		case 'REMOVE_SKILL':
-			return removeSkill(draft, action)
-
-		case 'REMOVE_SKILL_FROM_PROJECT':
-			return removeSkillFromProject(draft, action)
+			draft.allSkills = draft.allSkills.map(skill => {
+				if (skill.id === action.skillId) skill.tools.items.push(action.tool)
+				return skill
+			})
+			return
 
 		case 'REMOVE_CATEGORY':
-			return removeCategory(draft, action)
+			draft.allCategories = draft.allCategories
+				.map(category => {
+					if (category.id === action.categoryId) return null
+					return category
+				})
+				.filter(category => category !== null)
+
+			draft.allSkills = draft.allSkills.map(skill => {
+				if (skill.category && skill.category.id === action.categoryId) skill.category = null
+				return skill
+			})
+			return
+
+		case 'REMOVE_SKILL':
+			draft.projects = draft.projects.map(project => {
+				project.skills.items = project.skills.items
+					.map(skill => {
+						if (skill && skill.skillId === action.skillId) return null
+						return skill
+					})
+					.filter(skill => skill !== null)
+
+				return project
+			})
+			return
+
+		case 'REMOVE_SKILL_FROM_PROJECT':
+			draft.projects = draft.projects.map(project => {
+				if (project.id === action.skill.project.id) {
+					project.skills.items = project.skills.items.filter(item => item.id !== action.skill.id)
+				}
+				return project
+			})
+			return
 
 		case 'REMOVE_TOOL':
-			return removeTool(draft, action)
+			draft.allSkills = draft.allSkills.map(skill => {
+				const toolIndex = skill.tools.items.findIndex(i => i.id === action.toolId)
+				if (toolIndex > -1) skill.tools.items.splice(toolIndex, 1)
+				return skill
+			})
+			return
 
 		case 'SHOW_SPINNER':
 			return { ...draft, showSpinner: action.show }
@@ -44,149 +87,45 @@ const rootReducer = produce((draft = initialState, action) => {
 			delete action.type
 			return { ...draft, ...action }
 
-		case 'UPDATE_PROJECT':
-			return updateProject(draft, action)
-
 		case 'UPDATE_CATEGORY':
-			return updateCategory(draft, action)
+			draft.allCategories = draft.allCategories.map(i => {
+				if (i.id === action.category.id) return action.category
+				return i
+			})
 
-		case 'UPDATE_SKILL':
-			return updateSkill(draft, action)
-
-		case 'UPDATE_TOOL':
-			return updateTool(draft, action)
-
-		default:
-			return draft
-	}
-})
-
-function removeSkill(draft, { skillId }) {
-	const allSkills = draft.allSkills.filter(skill => skill.id !== skillId)
-
-	const projects = JSON.parse(JSON.stringify(draft.projects)).map(project => {
-		project.skills.items = project.skills.items
-			.map(skill => {
-				if (skill && skill.skillId === skillId) return null
+			draft.allSkills = draft.allSkills.map(skill => {
+				if (skill.category && skill.category.id === action.category.id)
+					skill.category = action.category
 				return skill
 			})
-			.filter(skill => skill !== null)
+			return
 
-		return project
-	})
+		case 'UPDATE_PROJECT':
+			draft.projects = draft.projects.map(draftProject => {
+				if (draftProject.id === action.project.id) return action.project
+				else return draftProject
+			})
+			return
 
-	return { ...draft, allSkills, projects }
-}
+		case 'UPDATE_SKILL':
+			draft.allSkills = draft.allSkills.map(skill =>
+				skill.id === action.updatedSkill.id ? action.updatedSkill : skill
+			)
+			return
 
-function addSkill(draft, { skill }) {
-	const allSkills = JSON.parse(JSON.stringify(draft.allSkills))
-	allSkills.push(skill)
+		case 'UPDATE_TOOL':
+			draft.allSkills = draft.allSkills.map(skill => {
+				if (skill.id === action.tool.skill.id) {
+					const items = skill.tools.items.map(i => (i.id === action.tool.id ? action.tool : i))
+					skill.tools.items = items
+				}
+				return skill
+			})
+			return
 
-	return { ...draft, allSkills }
-}
-
-function addCategory(draft, { category }) {
-	return { ...draft, allCategories: [...draft.allCategories, category] }
-}
-
-function updateCategory(draft, { category }) {
-	const allCategories = JSON.parse(JSON.stringify(draft.allCategories)).map(i => {
-		if (i.id === category.id) return category
-		return i
-	})
-
-	const allSkills = JSON.parse(JSON.stringify(draft.allSkills)).map(skill => {
-		if (skill.category && skill.category.id === category.id) skill.category = category
-
-		return skill
-	})
-
-	return { ...draft, allCategories, allSkills }
-}
-
-function removeCategory(draft, { categoryId }) {
-	const allCategories = JSON.parse(JSON.stringify(draft.allCategories))
-		.map(category => {
-			if (category.id === categoryId) return null
-			return category
-		})
-		.filter(category => category !== null)
-
-	const allSkills = JSON.parse(JSON.stringify(draft.allSkills)).map(skill => {
-		if (skill.category && skill.category.id === categoryId) skill.category = null
-		return skill
-	})
-
-	return { ...draft, allCategories, allSkills }
-}
-
-function removeTool(draft, { toolId }) {
-	const allSkills = JSON.parse(JSON.stringify(draft.allSkills)).map(skill => {
-		const toolIndex = skill.tools.items.findIndex(i => i.id === toolId)
-		if (toolIndex > -1) skill.tools.items.splice(toolIndex, 1)
-
-		return skill
-	})
-
-	return { ...draft, allSkills }
-}
-
-function removeSkillFromProject(draft, { skill }) {
-	const projects = JSON.parse(JSON.stringify(draft.projects)).map(project => {
-		if (project.id === skill.project.id) {
-			project.skills.items = project.skills.items.filter(item => item.id !== skill.id)
-		}
-		return project
-	})
-
-	return { ...draft, projects }
-}
-
-function addSkillToProject(draft, { skill }) {
-	const projects = JSON.parse(JSON.stringify(draft.projects)).map(project => {
-		if (project.id === skill.project.id) project.skills.items.push(skill)
-		return project
-	})
-
-	return { ...draft, projects }
-}
-
-function updateTool(draft, { tool }) {
-	const allSkills = JSON.parse(JSON.stringify(draft.allSkills)).map(skill => {
-		if (skill.id === tool.skill.id) {
-			const items = skill.tools.items.map(i => (i.id === tool.id ? tool : i))
-			skill.tools.items = items
-		}
-
-		return skill
-	})
-
-	return { ...draft, allSkills }
-}
-
-function updateSkill(draft, { updatedSkill }) {
-	const allSkills = JSON.parse(JSON.stringify(draft.allSkills)).map(skill =>
-		skill.id === updatedSkill.id ? updatedSkill : skill
-	)
-
-	return { ...draft, allSkills }
-}
-
-function addToolToSkill(draft, { skillId, tool }) {
-	const allSkills = JSON.parse(JSON.stringify(draft.allSkills)).map(skill => {
-		if (skill.id === skillId) skill.tools.items.push(tool)
-		return skill
-	})
-	return { ...draft, allSkills }
-}
-
-function updateProject(draft, { project }) {
-	const projects = [...draft.projects].map(draftProject => {
-		if (draftProject.id === project.id) return project
-		else return draftProject
-	})
-
-	return { ...draft, projects }
-}
+		default:
+			return
+	}
+})
 
 export default rootReducer
